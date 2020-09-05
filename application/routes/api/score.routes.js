9d3255bb4fd0bddf9b675f40ea679864d424a8b1
Francisco Angelim <frnangelim@gmail.com>
2020-09-05 18:34:00 -0300

    Ajuste na definicao da rota do put e delete

--- application/routes/api/score.routes.js
+++ application/routes/api/score.routes.js
@@ -7,7 +7,7 @@ let router = express.Router();
 router.get('/', Controller.getAll);
 router.get('/:id', Controller.getById);
 router.post('/', Controller.create);
-router.put('/', Controller.edit);
-router.delete('/', Controller.remove);
+router.put('/:id', Controller.edit);
+router.delete('/:id', Controller.remove);
 
 module.exports = router;
